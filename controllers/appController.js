eee0446852bc5220fa3d41de56de94092f5a108c
Honza Klimeš <johny.klimes@seznam.cz>
2023-03-25 13:20:28 +0100

    feat: Reading card name finished

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,4 +1,18 @@
-exports.uploadCard = (req, res) => {
-  console.log(req.body);
-  res.send("ESPER SENTINEL");
+exports.uploadCard = async (req, res) => {
+  const vision = require("@google-cloud/vision");
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  /**
+   * TODO(developer): Uncomment the following line before running the sample.
+   */
+  // const fileName = 'Local image file, e.g. /path/to/image.png';
+
+  // Performs text detection on the local file
+  const [result] = await client.textDetection(req.file.path);
+  const detections = result.textAnnotations;
+  const cardName = detections[0].description.split("\n")[0];
+
+  res.send(`Card uploaded - ${cardName}`);
 };
