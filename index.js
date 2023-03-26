a6c6d003f09200ce0d734f56ab777144c6290402
Honza Klimeš <johny.klimes@seznam.cz>
2023-03-26 21:11:27 +0200

    cors

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
-var cors = require("cors");
-app.use(cors());
 app.listen(PORT, () => {});
 
 module.exports = app;
