4e3fb8f96ca456a9a3daf381e84bb1c4cca4892c
Honza Klimeš <johny.klimes@seznam.cz>
2023-03-27 23:57:55 +0200

    fix: https request

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 var cors = require("cors");
 app.use(cors());
 app.use("/", routes);
-app.listen(PORT, () => {});
+const options = {
+  key: fs.readFileSync("certificates/private.key"),
+  cert: fs.readFileSync("certificates/certificate.crt"),
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server running on`);
+});
 
 module.exports = app;
